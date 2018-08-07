55216c454507f7b397f025664f3ea77f9f4f36dd
Gizem Akman Cuceoglu <gizema@spotify.com>
2018-08-07 16:32:29 +0200

    version 0.0.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import os
 import sys
 
 # circleci.py version
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
