974f60feb4d1eafa2af9a415dae3cf1ae320c5bb
Björn Hegerfors <bj0rn@spotify.com>
2018-09-03 13:47:05 +0200

    Remove unnecessary debug print

--- cstar/cstarcli.py
+++ cstar/cstarcli.py
@@ -69,7 +69,6 @@ def execute_cleanup(args):
 
 
 def execute_command(args):
-    cstar.output.debug(args)
     command = args.command
     if bool(args.seed_host) + bool(args.host) + bool(args.host_file) != 1:
         error("Exactly one of --seed-host, --host and --host-file must be used", print_traceback=False)
