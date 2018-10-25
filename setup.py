b34ee1c1e346b71013a784052997e8b33a13875d
Björn Hegerfors <bj0rn@spotify.com>
2018-10-25 16:17:07 +0200

    0.7.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import os
 import sys
 
 # circleci.py version
-VERSION = '0.7.2'
+VERSION = '0.7.3'
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
